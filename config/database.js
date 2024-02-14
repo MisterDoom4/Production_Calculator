f06125620940fd35b1197be19bc2aef950c69e30
MisterDoom4 <supervbarbosa@gmail.com>
2024-02-14 11:19:37 -0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// variavel de acesso ao banco de dados
+
+const dbAcess = process.env.MONGO_USER + encodeURIComponent(process.env.MONGO_PASS) + process.env.MONGO_DB;
+module.exports = {
+    mongoURI: dbAcess
+}
+
---
+++ index.js
@@ -0,0 +1,26 @@
+// Code to connect to MongoDB and start the server
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
+
+// Load environment variables
+require('dotenv').config();
+
+// Connect to MongoDB
+const databaseUrl = require('./config/database').mongoURI;
+mongoose.connect(databaseUrl, {
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
