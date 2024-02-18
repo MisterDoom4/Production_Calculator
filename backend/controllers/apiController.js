7854d598e62715f9dca2c1a21ef86b797cbd5cce
MisterDoom4 <supervbarbosa@gmail.com>
2024-02-18 16:26:32 -0300

    add recipe functions

--- backend/controllers/apiController.js
+++ backend/controllers/apiController.js
@@ -1,5 +1,27 @@
 const { Recipe, Item, Product } = require("../models/models");
 
+exports.getProduct = function (req, res, next) {
+  let query = req.params.id ? { _id: req.params.id } : {};
+  Product.find(query)
+    .select("name quantity unity cost")
+    .then((resp) => {
+      res.send(resp);
+    })
+    .catch((next) => {
+      res.send("id not found");
+    });
+};
+
+exports.showProducts = function (req, res, next) {
+  Product.find()
+    .then((resp) => {
+      res.send(resp);
+    })
+    .catch((next) => {
+      res.send(next);
+    });
+};
+
 exports.getRecipe = function (req, res, next) {
   let query = req.params.id ? { _id: req.params.id } : {};
   Recipe.find(query)
@@ -22,77 +44,88 @@ exports.showRecipes = function (req, res, next) {
     });
 };
 
-exports.getItem = function (req, res, next) {
-    let query = req.params.id ? { _id: req.params.id } : {};
-    Item.find(query)
-        .select("name quantity unity cost")
+exports.addRecipe = function (req, res, next) {
+  const recipe = new Recipe(req.body);
+  recipe
+    .save()
+    .then((resp) => {
+      res.send(resp);
+    })
+    .catch((next) => {
+      res.send(next);
+    });
+};
+
+exports.deleteRecipe = function (req, res, next) {
+    Recipe.findByIdAndDelete(req.params.id)
         .then((resp) => {
         res.send(resp);
         })
         .catch((next) => {
-        res.send("id not found");
+        res.send(next);
         });
     };
+
+exports.updateRecipe = function (req, res, next) {
+    Recipe.findByIdAndUpdate(req.params.id, req.body)
+        .then((resp) => {
+        res.send(resp);
+        })
+        .catch((next) => {
+        res.send(next);
+        });
+    };
+
+exports.getItem = function (req, res, next) {
+  let query = req.params.id ? { _id: req.params.id } : {};
+  Item.find(query)
+    .select("name quantity unity cost")
+    .then((resp) => {
+      res.send(resp);
+    })
+    .catch((next) => {
+      res.send("id not found");
+    });
+};
 
 exports.showItems = function (req, res, next) {
-    Item.find()
-        .then((resp) => {
-        res.send(resp);
-        })
-        .catch((next) => {
-        res.send(next);
-        });
-    };
-
-exports.getProduct = function (req, res, next) {
-    let query = req.params.id ? { _id: req.params.id } : {};
-    Product.find(query)
-        .select("name quantity unity cost")
-        .then((resp) => {
-        res.send(resp);
-        })
-        .catch((next) => {
-        res.send("id not found");
-        });
-    };
-
-exports.showProducts = function (req, res, next) {
-    Product.find()
-        .then((resp) => {
-        res.send(resp);
-        })
-        .catch((next) => {
-        res.send(next);
-        });
-    };
+  Item.find()
+    .then((resp) => {
+      res.send(resp);
+    })
+    .catch((next) => {
+      res.send(next);
+    });
+};
 
 exports.addItem = function (req, res, next) {
-    const item = new Item(req.body);
-    item.save().then((resp) => {
-        res.send(resp);
-    }).catch((next) => {
-        res.send(next);
+  const item = new Item(req.body);
+  item
+    .save()
+    .then((resp) => {
+      res.send(resp);
+    })
+    .catch((next) => {
+      res.send(next);
     });
 };
 
 exports.deleteItem = function (req, res, next) {
-    Item.findByIdAndDelete(req.params.id)
-        .then((resp) => {
-        res.send(resp);
-        })
-        .catch((next) => {
-        res.send(next);
-        });
+  Item.findByIdAndDelete(req.params.id)
+    .then((resp) => {
+      res.send(resp);
+    })
+    .catch((next) => {
+      res.send(next);
+    });
 };
 
 exports.updateItem = function (req, res, next) {
-    Item.findByIdAndUpdate(req.params.id, req.body)
-        .then((resp) => {
-        res.send(resp);
-        })
-        .catch((next) => {
-        res.send(next);
-        });
-}
-
-
+  Item.findByIdAndUpdate(req.params.id, req.body)
+    .then((resp) => {
+      res.send(resp);
+    })
+    .catch((next) => {
+      res.send(next);
+    });
+};
